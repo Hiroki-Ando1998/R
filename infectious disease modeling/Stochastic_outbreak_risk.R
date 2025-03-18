008512a065d173c8c82e732c95744ecd3073c754
Hiroki Ando <hirokiando.res@gmail.com>
2025-03-18 13:14:36 -0700

    Update Stochastic_outbreak_risk.R

--- infectious disease modeling/Stochastic_outbreak_risk.R
+++ infectious disease modeling/Stochastic_outbreak_risk.R
@@ -3,15 +3,15 @@
 solve_P <- function(a, R) {
   f <- function(P) (1 - P) * (1 + (R * P) / a)^a - 1
   
-  # P = 0 は自明な解なので除外し、Pの範囲を(1e-6, 1-1e-6) にする
+  # P = 0 is obvious answer. The range of P is set (1e-8, 1-1e-8)
   result <- tryCatch({
-    uniroot(f, lower = 1e-6, upper = 1 - 1e-6, tol = 1e-9)$root
-  }, error = function(e) NA)  # 解がない場合はNAを返す
+    uniroot(f, lower = 1e-8, upper = 1 - 1e-8, tol = 1e-9)$root
+  }, error = function(e) NA)  # if there is no answer, NA
   
   return(result)
 }
 
-# 例: a = 1, R = 1.5 の場合
+# for instance: a = 1, R = 1.5
 a <- 3
 R <- 3
 P_solution <- solve_P(a, R)
