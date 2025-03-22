2adb55370ff5212e9528958a181dd5b3b78c4a13
Hiroki Ando <hirokiando.res@gmail.com>
2025-03-22 14:28:16 -0700

    Update 1_B_Chi_Fisher_Mcneman.R

--- Statistical tests & epidemiological study design/1_B_Chi_Fisher_Mcneman.R
+++ Statistical tests & epidemiological study design/1_B_Chi_Fisher_Mcneman.R
@@ -1,1 +1,7 @@
+#χ2 test
+chisq.test(matrix)
 
+#Fisher exact test
+matrix(c(103, 43, 33, 117), nrow=2, byrow = TRUE,
+dimnames =list(Season = c("summer", "winter"), Test =c("Positive", "Negative")))
+fisher.test(matrix)
