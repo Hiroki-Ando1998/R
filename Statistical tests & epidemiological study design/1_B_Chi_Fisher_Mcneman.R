dd273a9d33dad1c44399bdce44c102f1aa7690ee
Hiroki Ando <hirokiando.res@gmail.com>
2025-03-22 14:28:47 -0700

    Update 1_B_Chi_Fisher_Mcneman.R

--- Statistical tests & epidemiological study design/1_B_Chi_Fisher_Mcneman.R
+++ Statistical tests & epidemiological study design/1_B_Chi_Fisher_Mcneman.R
@@ -5,3 +5,12 @@ chisq.test(matrix)
 matrix(c(103, 43, 33, 117), nrow=2, byrow = TRUE,
 dimnames =list(Season = c("summer", "winter"), Test =c("Positive", "Negative")))
 fisher.test(matrix)
+
+
+#McNemar’s test
+before <- c(89, 5)
+after <- c(16, 90)
+data_table <- matrix(c(before, after), nrow = 2, byrow = TRUE,
+                     dimnames = list(Season = c("effective", "ineffective"),
+                                     Test = c("effective", "ineffective")))
+mcnemar.test(data_table)
