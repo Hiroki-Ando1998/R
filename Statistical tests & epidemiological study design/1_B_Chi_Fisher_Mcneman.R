089681148c8c5dd0f6e6ab8479c734ae29c34816
Hiroki Ando <hirokiando.res@gmail.com>
2025-03-22 14:41:27 -0700

    Update 1_B_Chi_Fisher_Mcneman.R

--- Statistical tests & epidemiological study design/1_B_Chi_Fisher_Mcneman.R
+++ Statistical tests & epidemiological study design/1_B_Chi_Fisher_Mcneman.R
@@ -1,11 +1,20 @@
 #χ2 test
 chisq.test(matrix)
 
+example <0 matrix(c(74, 502, 27, 506), nrow=2, byrow = TRUE)
+chisq <- chisq.test(example, correct = FALSE)
+
+#Minimum expected should be over 5
+chisq$expected
+chisq$observed
+
 #effecti size for categorical data
 condition1 <- c(30, 20, 50)
 condition2 <- c(35, 30, 35)
 x <- cbind( condition1, condition2 )
 cramersV( x )
+
+
 
 #Fisher exact test
 matrix(c(103, 43, 33, 117), nrow=2, byrow = TRUE,
@@ -20,3 +29,12 @@ data_table <- matrix(c(before, after), nrow = 2, byrow = TRUE,
                      dimnames = list(Season = c("effective", "ineffective"),
                                      Test = c("effective", "ineffective")))
 mcnemar.test(data_table)
+
+library(exact2x2)
+sample <- matrix(c(0, 6, 1, 169), nrow=2,
+               dimnames = list("Cases" = c("Beautician", "Non-beaauticitian"),
+               "Control" = c("Beautician", "Non-beasutician")),byrow = TRUE)
+example <- matrix(c(74, 502, 27, 506), nrow=2, byrow = TRUE)
+
+mc <- mcnemar.test(example, correct = FALSE)
+mc_1 <- mcnemar.exact(example)
