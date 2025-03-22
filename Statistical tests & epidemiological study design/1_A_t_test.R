4c813eb91e1e1ee324fc84883ec89e8543ab0029
Hiroki Ando <hirokiando.res@gmail.com>
2025-03-22 14:28:07 -0700

    Update 1_A_t_test.R

--- Statistical tests & epidemiological study design/1_A_t_test.R
+++ Statistical tests & epidemiological study design/1_A_t_test.R
@@ -11,13 +11,7 @@ cliff.delta(data, data)
 cohens.d(data, data)
 herges_g()
 
-#χ2 test
-chisq.test(matrix)
 
-#Fisher exact test
-matrix(c(103, 43, 33, 117), nrow=2, byrow = TRUE,
-dimnames =list(Season = c("summer", "winter"), Test =c("Positive", "Negative")))
-fisher.test(matrix)
 
 #likelihood
 lr.test(A, B)
