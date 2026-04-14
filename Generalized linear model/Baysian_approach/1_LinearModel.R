20596128fe3ffa5ca30220c1ccb5c5310bc7df27
Hiroki Ando <hirokiando.res@gmail.com>
2026-04-13 21:40:47 -0700

    Create 1_LinearModel.R

---
+++ Generalized linear model/Baysian_approach/1_LinearModel.R
@@ -0,0 +1,31 @@
+
+library(dplyr)
+library(tidyr)
+library(plyr)
+library(rstan)
+library(ggplot2)
+#library(cowplot)
+
+R code
+#並列計算するにはstan関数やstan_model関数とsampling関数を呼ぶ前に以下のコードを書く
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+
+
+data_list_ww <- list(N = sample_size, x = data$infected, y = concentation)
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+mcmc <- stan(
+  file = "linear_regression.stan", 
+  data = data_list_ww,
+  seed = 1,
+  chain = 4,
+  iter = 10000,
+  warmup = 5000,
+  thin = 4
+)
+
+
