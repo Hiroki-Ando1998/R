94099de26ae5a3ddcff84df39f8fed09d83d96cc
Hiroki Ando <hirokiando.res@gmail.com>
2026-04-13 20:19:17 -0700

    Update geom_boxplot.R

--- ggplot2/geom_boxplot.R
+++ ggplot2/geom_boxplot.R
@@ -1,16 +1,19 @@
+
 plot <- ggplot(data_all_sample, aes(x = as.factor(COUNTY), y = adjust_cidm, fill = as.factor(sample))) + geom_boxplot() +
-  labs(x = "County", y = "Concentration (ug/g or ug/L)")
-plot <- plot + scale_fill_manual(values = c("lightblue", "orange", "red"))
-plot <- plot + theme_classic()
-plot <- plot + theme(
-  axis.text.x = element_text(angle = 45, hjust = 1),
-  axis.line = element_line(size = 1.0, lineend = "square"),
-  text = element_text(colour ="black", size = 14),
-  legend.position = c(0.28, 0.90), #"top
-  legend.text = element_text(size = 14),
-  legend.key.size = unit(3, "lines"),
-  legend.direction = "horizontal",
-  axis.ticks = element_line(linewidth = 1.5),
-  axis.ticks.length = unit(-2, "mm"))
-plot <- plot + guides(fill = guide_legend(title = NULL))
-plot
+labs(x = "County", y = "Concentration (ug/g or ug/L)") + 
+scale_fill_manual(values = c("lightblue", "orange", "red")) +
+  theme_minimal() +
+  theme(
+    #axis.title.x  = element_blank(),     
+    #axis.text.x   = element_blank(),    
+    #axis.ticks.x  = element_blank(),     
+    axis.title.y  = element_blank(), 
+    axis.line = element_line(linewidth = 0.5, lineend = "square"),
+    #axis.line.x   = element_blank(), 
+    text = element_text(colour = "black", size = 11),
+    legend.position = "none",
+    axis.ticks = element_line(linewidth = 0.5),
+    axis.ticks.length = unit(-2, "mm")
+  ) + 
+guides(fill = guide_legend(title = NULL))
+print(plot)
