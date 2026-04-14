3fe8a28a26f810a93cc82f271480ac1a9a786ea6
Hiroki Ando <hirokiando.res@gmail.com>
2026-04-13 21:09:02 -0700

    Update and rename 3_facet.R to 3_scale_fill_colour_shape.R

---
+++ ggplot2/3_scale_fill_colour_shape.R
@@ -0,0 +1,58 @@
+
+scale_colour_manual(
+  ...,
+  values,
+  aesthetics = "colour",
+  breaks = waiver(),
+  na.value = "grey50"
+)
+
+scale_fill_manual(
+  ...,
+  values,
+  aesthetics = "fill",
+  breaks = waiver(),
+  na.value = "grey50"
+)
+
+scale_size_manual(
+  ...,
+  values,
+  breaks = waiver(),
+  na.value = NA,
+  aesthetics = "size"
+)
+
+scale_shape_manual(
+  ...,
+  values,
+  breaks = waiver(),
+  na.value = NA,
+  aesthetics = "shape"
+)
+
+scale_linetype_manual(
+  ...,
+  values,
+  breaks = waiver(),
+  na.value = NA,
+  aesthetics = "linetype"
+)
+
+scale_linewidth_manual(
+  ...,
+  values,
+  breaks = waiver(),
+  na.value = NA,
+  aesthetics = "linewidth"
+)
+
+scale_alpha_manual(
+  ...,
+  values,
+  breaks = waiver(),
+  na.value = NA,
+  aesthetics = "alpha"
+)
+
+scale_discrete_manual(aesthetics, ..., values, breaks = waiver())
